15ed012d05357c4d456c0e9b15139216d3507e41
gbragafibra <gbragafibra@protonmail.com>
2024-07-01 11:57:36 +0100

    xavier weight init

--- GNN_tests/models.py
+++ GNN_tests/models.py
@@ -12,8 +12,8 @@ class GCN(nn.Module):
 		self.conv1 = GCNConv(4, h_dim)
 		self.conv2 = GCNConv(h_dim, h_dim)
 		self.conv3 = GCNConv(h_dim, h_dim)
-		self.conv4 = GCNConv(h_dim, h_dim)
-		self.fc = nn.Linear(h_dim, 1)
+		self.fc = nn.Linear(h_dim, 1, bias=False)
+		nn.init.xavier_uniform_(self.fc.weight)
 
 	def forward(self, input_):
 		x, e = input_.x, input_.edge_index
@@ -23,8 +23,7 @@ class GCN(nn.Module):
 		x = F.relu(x)
 		x = self.conv3(x, e)
 		x = F.relu(x)
-		x = self.conv4(x, e)
-		x = F.relu(x)
+		x = F.dropout(x, p = 0.5, training = self.training)
 		x = global_mean_pool(x, input_.batch)
 		x = self.fc(x)
 		x = F.sigmoid(x)
@@ -34,13 +33,24 @@ class GCN(nn.Module):
 class GIN(nn.Module):
 	def __init__(self, h_dim):
 		super().__init__()
-		self.conv1 = GINConv(nn.Sequential(nn.Linear(4, h_dim),
+		self.conv1 = GINConv(nn.Sequential(nn.Linear(4, h_dim, bias=False),
 			nn.BatchNorm1d(h_dim), nn.ReLU()))
-		self.conv2 = GINConv(nn.Sequential(nn.Linear(h_dim, h_dim),
+		self.conv2 = GINConv(nn.Sequential(nn.Linear(h_dim, h_dim, bias=False),
 			nn.BatchNorm1d(h_dim), nn.ReLU()))
-		self.conv3 = GINConv(nn.Sequential(nn.Linear(h_dim, h_dim),
+		self.conv3 = GINConv(nn.Sequential(nn.Linear(h_dim, h_dim, bias=False),
 			nn.BatchNorm1d(h_dim), nn.ReLU()))
-		self.fc = nn.Linear(h_dim, 1)
+		self.fc = nn.Linear(h_dim, 1, bias=False)
+
+		self._initialize_weights
+
+	def _initialize_weights():
+		def init_linear(self):
+			if isinstance(layer, nn.Linear):
+				nn.init.xavier_uniform_(layer.weight)
+		self.conv1.apply(init_linear)
+		self.conv2.apply(init_linear)
+		self.conv3.apply(init_linear)
+		init_linear(self.fc)
 
 	def forward(self, input_):
 		x, e = input_.x, input_.edge_index
@@ -48,6 +58,7 @@ class GIN(nn.Module):
 		h = self.conv1(x, e)
 		h = self.conv2(h, e)
 		h = self.conv3(h, e)
+		h = F.dropout(h, p = 0.5, training = self.training)
 		h = global_mean_pool(h, input_.batch)
 		h = self.fc(h)
 		h = F.sigmoid(h)
